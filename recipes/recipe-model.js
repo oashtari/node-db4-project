75e8ed7ad7cd28c8571e9206c1c2ecb6bbf8482b
oashtari <oashtari@gmail.com>
2020-04-16 08:58:47 -0700

    set up tables migrations and corresponding seed files

--- recipes/recipe-model.js
+++ recipes/recipe-model.js
@@ -1,5 +1,70 @@
 const db = require('../data/db-config');
 
 module.exports = {
-
+    getRecipes,
+    getShoppingList,
+    getInstructions
 }
+
+function getRecipes() {
+
+};
+
+function getShoppingList(id) {
+
+};
+
+function getInstructions(id) {
+
+};
+
+
+// In addition to the `migrations` and `seeding` scripts, write a data access file that **exports** an object with the following functions:
+
+// - `getRecipes()`: should return a list of all recipes in the database.
+// - `getShoppingList(recipe_id)`: should return a list of all ingredients and quantities for a given recipe
+// - `getInstructions(recipe_id)`: should return a list of step by step instructions for preparing a recipe
+
+// Organize and name your files anyway you see fit.
+
+
+// function find() {
+//     return db('schemes');  //
+// }
+
+// function findById(id) {
+//     return db('schemes')
+//         .where({ id })
+//         .first()
+// }
+
+// function findSteps(id) {
+//     return db('schemes')
+//         .join('steps', 'schemes.id', '=', 'steps.scheme_id')
+//         .select('schemes.scheme_name', 'steps.step_number', 'steps.instructions')
+//         .where({ 'schemes.id': id })
+//         .orderBy(['scheme_name', { column: 'step_number', order: 'asc' }])
+// }
+
+// function add(scheme) {
+//     return db('schemes')
+//         .insert(scheme, 'id')
+//         .then(id => {
+//             return findById(id[0])
+//         })
+// }
+
+// function update(changes, id) {
+//     return db('schemes')
+//         .where({ id })
+//         .update(changes, 'id')
+//         .then(() => { // if you want to return the item, this is not needed
+//             return findById(id)
+//         })
+// }
+
+// function remove(id) {
+//     return db('schemes')
+//         .where({ id })
+//         .del();
+// }
--- recipes/recipe-router.js
+++ recipes/recipe-router.js
@@ -9,3 +9,134 @@ const router = express.Router();
 
 
 module.exports = router;
+
+
+
+
+// Build the following endpoints. Write any additional data access helpers as needed.
+
+// - `GET /api/recipes/`: all recipes (without details about ingredients or steps)
+// - `GET /api/recipes/:id/shoppingList`: a list of ingredients and quantites for a single recipe
+// - `GET /api/recipes/:id/instructions`: a correctly ordered list of how to prepare a single recipe
+// - `GET /api/ingredients/:id/recipes`: all recipes in the system that utilize a single ingredient 
+
+
+// const express = require('express');
+
+// // const db = require('../data/db-config.js');
+// const Schemes = require('./scheme-model.js');
+
+// const router = express.Router();
+
+// router.get('/', (req, res) => {
+//   Schemes.find()
+//     .then(schemes => {
+//       res.json(schemes);
+//     })
+//     .catch(err => {
+//       res.status(500).json({ message: 'Failed to get schemes' });
+//     });
+// });
+
+// router.get('/:id', (req, res) => {
+//   const { id } = req.params;
+
+//   Schemes.findById(id)
+//     .then(scheme => {
+//       if (scheme) {
+//         res.json(scheme);
+//       } else {
+//         res.status(404).json({ message: 'Could not find scheme with given id.' })
+//       }
+//     })
+//     .catch(err => {
+//       res.status(500).json({ message: 'Failed to get schemes' });
+//     });
+// });
+
+// router.get('/:id/steps', (req, res) => {
+//   const { id } = req.params;
+
+//   Schemes.findSteps(id)
+//     .then(steps => {
+//       if (steps.length) {
+//         res.json(steps);
+//       } else {
+//         res.status(404).json({ message: 'Could not find steps for given scheme' })
+//       }
+//     })
+//     .catch(err => {
+//       res.status(500).json({ message: 'Failed to get steps' });
+//     });
+// });
+
+// router.post('/', (req, res) => {
+//   const schemeData = req.body;
+
+//   Schemes.add(schemeData)
+//     .then(scheme => {
+//       console.log(scheme)
+//       res.status(201).json(scheme);
+//     })
+//     .catch(err => {
+//       res.status(500).json({ message: 'Failed to create new scheme' });
+//     });
+// });
+
+// router.post('/:id/steps', (req, res) => {
+//   const stepData = req.body;
+//   const { id } = req.params;
+
+//   Schemes.findById(id)
+//     .then(scheme => {
+//       if (scheme) {
+//         Schemes.addStep(stepData, id)
+//           .then(step => {
+//             res.status(201).json(step);
+//           })
+//       } else {
+//         res.status(404).json({ message: 'Could not find scheme with given id.' })
+//       }
+//     })
+//     .catch(err => {
+//       res.status(500).json({ message: 'Failed to create new step' });
+//     });
+// });
+
+// router.put('/:id', (req, res) => {
+//   const { id } = req.params;
+//   const changes = req.body;
+
+//   Schemes.findById(id)
+//     .then(scheme => {
+//       if (scheme) {
+//         Schemes.update(changes, id)
+//           .then(updatedScheme => {
+//             res.json(updatedScheme);
+//           });
+//       } else {
+//         res.status(404).json({ message: 'Could not find scheme with given id' });
+//       }
+//     })
+//     .catch(err => {
+//       res.status(500).json({ message: 'Failed to update scheme' });
+//     });
+// });
+
+// router.delete('/:id', (req, res) => {
+//   const { id } = req.params;
+
+//   Schemes.remove(id)
+//     .then(deleted => {
+//       if (deleted) {
+//         res.json({ removed: deleted });
+//       } else {
+//         res.status(404).json({ message: 'Could not find scheme with given id' });
+//       }
+//     })
+//     .catch(err => {
+//       res.status(500).json({ message: 'Failed to delete scheme' });
+//     });
+// });
+
+// module.exports = router;
